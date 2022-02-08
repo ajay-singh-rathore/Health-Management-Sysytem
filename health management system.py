4ece298650ca22c788e3960aecfccef84295a145
AJAY_SINGH_RATHORE <96069023+ajay-singh-rathore@users.noreply.github.com>
2022-02-08 21:26:58 +0530

    Update health management system.py

--- health management system.py
+++ health management system.py
@@ -9,36 +9,36 @@ def type(k):
 
         if c==1:
             value = str(input("Type here : "))
-            with open("Harry-ex.txt","a") as op:
+            with open("Ajay-ex.txt","a") as op:
                 op.write(str([str(datetime())])+": "+value+"\n")
             print("<----Sucessfully written---->")
         if c==2:
             value = str(input("Type here : "))
-            with open("Harry-food.txt","a") as op:
+            with open("Ajay-food.txt","a") as op:
                 op.write(str([str(datetime())])+": "+value+"\n")
             print("<---Sucessfully Written---->")
     elif k==2:
         c=int(input("Enter 1 for exercise \n Enter 2 for Diet : "))
         if c==1:
             value = str(input("Type here : "))
-            with open("Rohan-ex.txt","a") as op:
+            with open("Vicky-ex.txt","a") as op:
                 op.write(str([str(datetime())])+": "+value+"\n")
             print("<----sucessfully written---->")
         elif c==2:
             value = str(input("Type here : "))
-            with open("Rohan-food.txt","a") as op:
+            with open("Vicky-food.txt","a") as op:
                 op.write(str([str(datetime())])+": "+value+"\n")
             print("<----Sucessfully written---->")
     elif k==3:
         c=int(input("Enter 1 for exercise \n Enter 2 for Diet : "))
         if c==1:
             value = str(input("Type here : "))
-            with open("Hammad-ex.txt","a") as op:
+            with open("vaibhav-ex.txt","a") as op:
                 op.write(str([str(datetime())])+": "+value+"\n")
             print("<----Sucessfully Written---->")
         elif c==2:
             value = str(input("Type here : "))
-            with open("Hammad-food.txt","a") as op:
+            with open("vaibhav-food.txt","a") as op:
                 op.write(str([str(datetime())])+": "+value+"\n")
     else:
         print("sorry! Invalid option : ----> ")
@@ -48,32 +48,32 @@ def retrieve(k):
         c=int(input("Enter 1 for exercise \n Enter 2 for Diet : "))
         if c==1:
 
-            with open("Harry-ex.txt") as op:
+            with open("Ajay-ex.txt") as op:
                 for i in op:
                     print(i,end=" ")
         elif c==2:
 
-            with open("Harry-food.txt") as op:
+            with open("Ajay-food.txt") as op:
                 for i in op:
                     print(i,end=" ")
     elif k==2:
         c=int(input("Enter 1 for exercise \n Enter 2 for Diet : "))
         if c==1:
-            with open("Rohan-ex.txt") as op:
+            with open("Vicky-ex.txt") as op:
                 for i in op:
                     print(i,end=" ")
         elif c==2:
-            with open("Rohan-food.txt") as op:
+            with open("Vicky-food.txt") as op:
                 for i in op:
                     print(i,end=" ")
     elif k==3:
         c=int(input("Enter 1 for exercise \n Enter 2 for Diet : "))
         if c==1:
-            with open("Hammad-ex.txt") as op:
+            with open("vaibhav-ex.txt") as op:
                 for i in op:
                     print(i,end=" ")
         elif c==2:
-            with open("Hammad-food.txt") as op:
+            with open("vaibhav-food.txt") as op:
                 for i in op:
                     print(i,end=" ")
     else:
@@ -81,11 +81,18 @@ def retrieve(k):
 print("<-=------------------HEALTH MANAGEMENT SYSTEM---------------------=>")
 a=int(input("Press 1 for log  Press 2 for Retrieve \n"))
 if a==1:
-    b=int(input("Press 1 for Harry \n Press 2 for Rohan \n Press 3 for Hammad \n"))
+    b=int(input("Press 1 for Ajay \n Press 2 for Vicky \n Press 3 for vaibhav \n"))
     type(b)
 else:
-    b=int(input("Press 1 for Harry \n Press 2 for Rohan \n Press 3 for Hammad \n"))
+    b=int(input("Press 1 for Ajay \n Press 2 for Vicky \n Press 3 for vaibhav \n"))
     retrieve(b)
+
+
+
+
+
+
+
 
 
 
